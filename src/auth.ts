db50ebb89fb866df778b1f2e5f4a973477c9be5d
PetrDavidLanca <petrdavidlanca@gmail.com>
2026-09-18 15:15:11 +0200

    Populate user ID in auth session

--- src/auth.ts
+++ src/auth.ts
@@ -42,4 +42,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     })
   ],
   session: { strategy: "jwt" },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (session.user && token.id) {
+        session.user.id = token.id as string
+      }
+      return session
+    }
+  }
 })
